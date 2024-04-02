644075683e30342497142b7edb97c224b36e378a
catherinesfred <154595459+catherinefrederic@users.noreply.github.com>
2024-04-02 21:04:05 +0700

    Add files via upload

---
+++ cathflower.py
@@ -0,0 +1,11 @@
+import turtle
+t = turtle.Turtle ()
+s = turtle.Screen ()
+s.bgcolor ("pink")
+t.pencolor ("purple")
+t.speed (0)
+for i in range (150):
+    t.circle(190-i,90)
+    t.lt(90)
+    t.circle(190-i,90)
+    t.lt(18)
